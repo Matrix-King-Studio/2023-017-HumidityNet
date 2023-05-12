0e24ae909b0f26ffdf544778c01e85e79d393f2f
liu_zhao_feng_PC <liu_zhao_feng_alex@163.com>
2023-05-12 19:32:32 +0800

    写了一个超简版

--- WEB/app/models.py
+++ WEB/app/models.py
@@ -10,7 +10,7 @@ class SensorData(models.Model):
     humidity = models.FloatField(verbose_name="湿度")
     water_level = models.FloatField(verbose_name="水位")
     wifi_status = models.BooleanField(verbose_name="WiFi状态")
-    spray_status = models.IntegerField(verbose_name="喷雾状态")
+    spray_status = models.IntegerField(verbose_name="喷雾状态", default=2)
 
     def __str__(self):
         return f"温度: {self.temperature:.1f},湿度: {self.humidity:.1f},水位: {self.water_level:.1f}"
--- WEB/app/views.py
+++ WEB/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from app.models import SensorData
@@ -10,6 +13,33 @@ class SensorDataViewSet(ModelViewSet):
     queryset = SensorData.objects.all().order_by("-id")
     serializer_class = SensorDataSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        request_data = {k: v for k, v in request.data.items()}
+        latest_spray_status = SensorData.objects.latest("id").spray_status
+        request_data["spray_status"] = latest_spray_status
+        serializer = self.get_serializer(data=request_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(methods=["post"], detail=False)
+    def change_spray_status(self, request):
+        spray_status = request.data.get("spray_status")
+        if spray_status is None:
+            return Response({"error": "spray_status parameter is missing"}, status=400)
+        latest_data = SensorData.objects.latest("id")
+        latest_data.spray_status = spray_status
+        latest_data.save()
+        serializer = SensorDataSerializer(latest_data)
+        return Response(serializer.data)
+
+    @action(methods=["get"], detail=False)
+    def get_latest_spray_status(self, request):
+        latest_data = SensorData.objects.latest("id")
+        spray_status = latest_data.spray_status
+        return Response({"sw": spray_status})
 
 
 def chart_view(request):
